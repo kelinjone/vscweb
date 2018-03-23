371e5c901022df5ba303f9a3557aa6bab1cb6c4f
战哥 <zhanyuan.lizhy@antfin.com>
2018-03-23 21:29:26 +0800

    临时提交

--- src/commands.js
+++ src/commands.js
@@ -4,23 +4,14 @@ var vscode = require("vscode");
 module.exports = function commands() {
   return openCommands.map(function (open) {
     return vscode.commands.registerCommand(open.command, open.handler);
-  })
+  });
 }
 
 var openCommands = [
-  { command: "vscweb.browser", handler: browse.bind(browser) },
   { command: "vscweb.chrome", handler: browse.bind(chrome) },
   { command: "vscweb.firefox", handler: browse.bind(firefox) },
   { command: "vscweb.opera", handler: browse.bind(opera) },
   { command: "vscweb.safari", handler: browse.bind(safari) }
-];
-
-var browses = [
-  { command: "browser", open: "open " },
-  { command: "chrome", open: 'open -a "/Applications/Google Chrome.app" -n --args ' },
-  { command: "firefox", open: "open -a Firefox " },
-  { command: "opera", open: "open -a Opera " },
-  { command: "safari", open: "open -a Safari " }
 ];
 
 function browse(resource) {
@@ -40,10 +31,6 @@ function browse(resource) {
       }
     }
   }
-}
-
-function browser(url) {
-  exec("open " + url);
 }
 
 function chrome(url) {
